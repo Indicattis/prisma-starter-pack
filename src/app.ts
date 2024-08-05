2bc63808e92e59c84ec43e22bce102e6e3835906
unknown <joao.staehler@gmail.com>
2024-08-05 13:29:48 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+// import { routes } from './routes';
+export const app = express();
+export const port = 3333;
+
+app.use(express.json());
+app.use(cors());
+// app.use(routes)
+
+app.get('/', (req, res) => {
+  return res.status(201).send("Servidor online!");
+});
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import { app, port } from "./app";
+
+
+
+app.listen(port, () => {
+    console.log("HTTP server running on port "+port)
+})
